00c3052f886e1f77e607d2bb29e6d40d44810ad1
danjhkim <danjhkim@gmail.com>
2021-02-26 23:40:12 -0500

    Few React hook based reusable widgets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
+import Search from './components/Search'
+import Translate from './components/Translate'
+import Route from './components/Route'
+import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a frontend javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favourite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use react by creating components'
+    }
+]
+
+const options = [
+    {
+        title: 'The Color Red',
+        value: 'red'
+    },
+    {
+        title: 'The Color Green',
+        value: 'green'
+    },
+    {
+        title: 'The color Blue',
+        value: 'blue'
+    }
+]
+
+
+const App = () => {
+    const [selected, setSelected] = useState([]);
+
+    return (
+        <div className="ui container">
+            <Header />
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    options={options}
+                    title={'Select a Color'}
+                    selected={selected}
+                    setSelected={setSelected}
+                />
+            </Route>
+        </div>
+
+    );
 }
 
 export default App;
